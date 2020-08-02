56934bab44de4f07ca8f6404e44fd1169fe3c8ef
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-08-01 21:25:17 -0700

    added test for manager and passed

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,2 +1,14 @@
+const Manager = require('../lib/Manager');
 
+test("create manager object", () => {
+    const manager = new Manager("Michelle", 1908, "michelle@test.com", 890);
+
+    expect(manager.officeNumber).toEqual(expect.any(Number));
+});
+
+test("getRole() return Manager", () => {
+    const manager = new Manager("Michelle", 1908, "michelle@test.com", 890);
+
+    expect(manager.getRole()).toBe(Manager);
+});
 
