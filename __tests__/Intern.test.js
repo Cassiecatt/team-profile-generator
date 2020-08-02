5db55dbd9a8ce4a53d60cb28163fa70d1e5162aa
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-08-01 21:32:34 -0700

    added intern test and passed

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,13 @@
+const Intern = require('../lib/Intern');
+
+test("create intern object", () => {
+    const intern = new Intern("Sara", 8970, "sara@test.com", "UofA");
+
+    expect(intern.school).toEqual(expect.any(String));
+});
+
+test("getRole() return Intern", () => {
+    const intern = new Intern("Sara", 8970, "sara@test.com", "UofA");
+    
+    expect(intern.getRole()).toBe(Intern);
+});
