56dc0d5bae905827e14b28ea9e90009dd847c350
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-08-02 16:16:59 -0700

    updated a href on generate html

--- src/generate-html.js
+++ src/generate-html.js
@@ -70,7 +70,7 @@ function generateEngineerHtml(engineer) {
       <ul class="list-item">
         <li class="list-group-item">ID: ${engineer.getId()}</li>
         <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
-        <li class="list-group-item">Github: <a href="https://github.com/${engineer.getGithub()}" target"_blank">${engineer.getGithub()}</a></li>
+        <li class="list-group-item">Github: <a href="https://github.com/${engineer.getGithub()}" target="_blank">${engineer.getGithub()}</a></li>
       </ul>
     </div>
   </div>
